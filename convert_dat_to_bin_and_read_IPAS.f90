c4bdd51eb0d84ddd71d20e969f671031ca22e646
Kara Sulia <ksulia@albany.edu>
2018-07-05 20:10:46 +0800

    coded IPAS lookup table for 1 variable

--- convert_dat_to_bin_and_read_IPAS.f90
+++ convert_dat_to_bin_and_read_IPAS.f90
@@ -4,60 +4,61 @@
         integer, parameter :: ii = 50, jj = 28, kk = 3
         real :: phi(ii),r(jj)
         real :: an(ii,jj),cn(ii,jj),a(ii,jj)
-        real var(ii+1,jj+1)
+        real var(ii,jj+1)
+
+        r(1) = 1
+        do j=2,jj
+           if(r(j-1).lt.10)then
+              r(j) = r(j-1) + 1
+           else if(r(j-1).ge.10.and.r(j-1).lt.100)then
+              r(j) = r(j-1) + 10
+           else if(r(j-1).ge.100)then
+              r(j) = r(j-1) + 100
+           end if
+        end do
 
         open(9,file='a_avg_lookup_new.txt')
-        do i=1,ii+1
-           do j=1,jj+1
-           read(9,*) var(i,j)
-           end do
+        do i=1,ii
+           read(9,*) (var(i,j),j=1,jj+1)
         end do
         close(9)
 
-        phi = var(2:ii+1,1)
-        r = var(1,1:jj)
-
         do i=1,ii
+           phi(i) = var(i,1)
            do j=1,jj
-              a(i,j) = var(i+1,j+1)
-           end do
-        end do
-
-        
-        
-!        open(8,file='COLL.bin',form='unformatted')
-
-!        WRITE(8) (r(i),i=1,ii)
-!        WRITE(8) (phi(j),j=1,jj)
-!        WRITE(8) (cn(k),k=1,kk)
-!        WRITE(8) (nu(l),l=1,ll)
-!        WRITE(8) (rho(m),m=1,mm)
-!        WRITE(8) (((((coll(i,j,k,l,m),i=1,ii),j=1,jj),k=1,kk),l=1,ll),m=1,mm)
-!        WRITE(8) (((((ncoll(i,j,k,l,m),i=1,ii),j=1,jj),k=1,kk),l=1,ll),m=1,mm)
-!        CLOSE(8)
-
-!        open(8,file='COLL.bin',form='unformatted')
-!        READ(8) (n(i),i=1,ii)
-!        READ(8) (a(j),j=1,jj)
-!        READ(8) (c(k),k=1,kk)
-!        READ(8) (nn(l),l=1,ll)
-!        READ(8) (r(m),m=1,mm)
-!        READ(8) (((((co(i,j,k,l,m),i=1,ii),j=1,jj),k=1,kk),l=1,ll),m=1,mm)
-!        READ(8) (((((nco(i,j,k,l,m),i=1,ii),j=1,jj),k=1,kk),l=1,ll),m=1,mm)
-!        CLOSE(8)
-
-        
-        do i=1,ii
-           do j=1,jj
-              !print*,i,j,var(i,j) 
-              write(*,*) i,j,phi(i),r(j),a(i,j)
-               
-
+              a(i,j) = var(i,j+1)    
            enddo
         enddo
         
+        OPEN(8,file='ACOLL.bin',form='unformatted')
+        WRITE(8) (phi(i),i=1,ii)
+        WRITE(8) (r(j),j=1,jj)
+        WRITE(8) ((a(i,j),i=1,ii),j=1,jj)
+        CLOSE(8)
+
+        phi=0
+        r=0
+        a=0
+
+        do i=1,ii
+           do j=1,jj   
+              print*,i,j,phi(i),r(j),a(i,j)
+           enddo
+        enddo
+
+
+        OPEN(8,file='ACOLL.bin',form='unformatted')
+        READ(8) (phi(i),i=1,ii)
+        READ(8) (r(j),j=1,jj)
+        READ(8) ((a(i,j),i=1,ii),j=1,jj)
+        CLOSE(8)
+
         
-        
+        do i=1,ii
+           do j=1,jj   
+              print*,i,j,phi(i),r(j),a(i,j)
+           enddo
+        enddo
         
       end program main
 

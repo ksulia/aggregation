3b5c57e1a7158fa297581ed746f7ee8ef1aa6470
Kara Sulia <ksulia@dendrite.asrc.albany.edu>
2019-05-14 17:01:22 -0400

    fixed bugs, optimized

--- collection_lookup_generator_large.f90
+++ collection_lookup_generator_large.f90
@@ -11,7 +11,7 @@ PROGRAM COLLECTION_LOOKUP_GENERATOR_LARGE
      real gammln, pi, gam, maxx, maxy, area, vol1, vol2, n1, n2, v1, v2
      real ncheck(nn,oo), mrate(qq,pp,oo,nn), nrate(qq,pp,oo,nn)
      real coll, ncoll, ni(ii), an(jj), cn(kk), nu(ll), rho(mm), a1(nn), c1(oo), a2(pp), c2(qq) 
-     real n3(pp)
+     real n3(pp), v(oo,nn)
      real start, finish
 
      integer ncid,vid,nid,aid,cid,nuid,rhoid
@@ -33,7 +33,16 @@ PROGRAM COLLECTION_LOOKUP_GENERATOR_LARGE
         !print*,r(n),dr(n)
      END DO
      !r = [0.5,1.0,1.5-->10,15,20,25-->100,150,200,250-->1000,---->100000] microns
-
+     DO n = 1, nn, 1
+         DO o = 1, oo, 1
+             maxx = max(r(n)*1.e6,r(o)*1.e6)
+             !!Heymsfield, Bansemer, and Schmitt 2013
+             if(2.*maxx .lt. 41) v(o,n) = 0.0028*(2.*maxx)**2.
+             if(2.*maxx .ge. 41 .and. 2.*maxx .lt. 839) v(o,n) = 0.0791*(2.*maxx)**1.101
+             if(2.*maxx .ge. 839) v(o,n) = 62.29*(2.*maxx)**0.1098
+             v(o,n) = v(o,n)/100.
+         END DO
+     END DO
 
      OPEN(1,FILE="COLLV4.dat")
      !$OMP PARALLEL DO
@@ -48,6 +57,7 @@ PROGRAM COLLECTION_LOOKUP_GENERATOR_LARGE
 
               DO l = 1,ll,1
                  nu(l) = real(l)
+                 gam = exp(gammln(nu(l)))
 
                  DO m = 1,mm,1
                     rho(m) = real(m)*100.
@@ -65,40 +75,30 @@ PROGRAM COLLECTION_LOOKUP_GENERATOR_LARGE
                        DO o = 1,oo,1
                          c1(o) = r(o)  *1.e-6
 
+                         maxx = max(a1(n),c1(o))
+                         v1 = v(o,n)
+                         vol1 = a1(n)*a1(n)*c1(o)
+                         n1 = ni(i)/gam*gam * (a1(n)/an(j))**(nu(l)-1.)*(exp(-a1(n)/an(j))/an(j)) *&
+                                                   (c1(o)/cn(k))**(nu(l)-1.)*(exp(-c1(o)/cn(k))/cn(k))
+
                          DO p = 1,pp,1
                             a2(p) = r(p)  *1.e-6
 
                             DO q = 1,qq,1
                                c2(q) = r(q)  *1.e-6
         
-                               gam = exp(gammln(nu(l)))  
-                               maxx = max(a1(n),c1(o))
                                maxy = max(a2(p),c2(q))    
-                               area = (maxx + maxy)**2
+                               area = (maxx + maxy)*(maxx + maxy)
+                               
+                               v2 = v(q,p)
+                               vol2 = a2(p)*a2(p)*c2(q)
 
-                               vol1 = a1(n)**2*c1(o)
-                               vol2 = a2(p)**2*c2(q)
-
-                               n1 = ni(i)/gam**2 * (a1(n)/an(j))**(nu(l)-1.)*(exp(-a1(n)/an(j))/an(j)) *&
-                                                   (c1(o)/cn(k))**(nu(l)-1.)*(exp(-c1(o)/cn(k))/cn(k))
-                               n2 = ni(i)/gam**2 * (a2(p)/an(j))**(nu(l)-1.)*(exp(-a2(p)/an(j))/an(j)) *&
+                               n2 = ni(i)/gam*gam * (a2(p)/an(j))**(nu(l)-1.)*(exp(-a2(p)/an(j))/an(j)) *&
                                                    (c2(q)/cn(k))**(nu(l)-1.)*(exp(-c2(q)/cn(k))/cn(k))
 
-                               !!Heymsfield, Bessimer, and Schmitt 2013
-                               if(2.*maxx .lt. 41) v1 = 0.0028*(2.*maxx)**2.
-                               if(2.*maxx .ge. 41 .and. 2.*maxx .lt. 839) v1 = 0.0791*(2.*maxx)**1.101
-                               if(2.*maxx .ge. 839) v1 = 62.29*(2.*maxx)**0.1098
-                               v1 = v1/100.
-
-                               if(2.*maxy .lt. 41) v2 = 0.0028*(2.*maxy)**2.
-                               if(2.*maxy .ge. 41 .and. 2.*maxy .lt. 839) v2 = 0.0791*(2.*maxy)**1.101
-                               if(2.*maxy .ge. 839) v2 = 62.29*(2.*maxy)**0.1098
-                               v2 = v2/100.
 
                                mrate(q,p,o,n) = pi*rho(m)*(vol1+vol2)*area*abs(v1-v2)*n1*n2*E*dr(n)*dr(o)*dr(q)*dr(p)
                                nrate(q,p,o,n) = pi*area*abs(v1-v2)*n1*n2*E*dr(n)*dr(o)*dr(q)*dr(p)
-                               ncheck(p,q) = n2
-                !               print*,dr(n),dr(o),dr(q),dr(p),dr(n)*dr(o)*dr(q)*dr(p)
                              END DO!c2(q)
 
                           END DO!a2(p)
@@ -122,7 +122,7 @@ PROGRAM COLLECTION_LOOKUP_GENERATOR_LARGE
 
      END DO!ni(i)
      !$OMP END PARALLEL DO
-  CLOSE(1)
+     CLOSE(1)
   
 !     coll = coll*pi  !!divide by air density in model!!
 
